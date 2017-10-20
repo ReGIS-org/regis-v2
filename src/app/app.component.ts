4d30b5d1c3f2f6b8b0495902a1c447b7a70cefb8
Carlos <c.martinez@esciencecenter.nl>
2017-10-20 16:41:30 +0200

    Make map 2D mercator by default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MapLayerProviderOptions } from 'angular-cesium/src/angular-cesium/models/map-layer-provider-options.enum';
+import { MapsManagerService } from 'angular-cesium/src/angular-cesium/services/maps-manager/maps-manager.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  planes = [];
+  MapLayerProviderOptions = MapLayerProviderOptions;
+  Cesium = Cesium;
+  mapsManagerService = MapsManagerService;
+  selectedSceneMode = Cesium.SceneMode.SCENE2D;
 }
