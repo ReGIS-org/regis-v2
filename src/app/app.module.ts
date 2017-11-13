98e264cdff1f62d0203a6d0f3227eda1f5e1b489
Maarten van Meersbergen <maartenvm@gmail.com>
2017-11-13 11:25:35 +0100

    Added material design lite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MdlModule } from '@angular-mdl/core';
 
 import { AppComponent } from './app.component';
 import { AngularCesiumModule } from 'angular-cesium';
 import { CesiumMapComponent } from './cesium-map/cesium-map.component';
 import { ProjectlogoComponent } from './projectlogo/projectlogo.component';
 import { MapsLayerComponent } from './map-layer/maps-layer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CesiumMapComponent,
     ProjectlogoComponent,
-    MapsLayerComponent
+    MapsLayerComponent,
+    MenuComponent
   ],
   imports: [
+    MdlModule,
     BrowserModule,
     AngularCesiumModule.forRoot()
   ],
