3d8e510a2565f74a4f893e5c80e547c129ba41b8
Maarten van Meersbergen <maartenvm@gmail.com>
2017-11-14 14:58:02 +0100

    merge

--- src/app/layer-selector/layer-selector.component.ts
+++ src/app/layer-selector/layer-selector.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterContentInit, ViewEncapsulation } from '@angular/core';
+import { MdlDialogComponent } from '@angular-mdl/core';
 import { LayerService } from '../services/layer-service';
 
+import { AddLayerModalComponent } from '../add-layer-modal/add-layer-modal.component';
 
 @Component({
   selector: 'app-layer-selector',
   templateUrl: './layer-selector.component.html',
   styleUrls: ['./layer-selector.component.css'],
   providers: [LayerService],
+  encapsulation: ViewEncapsulation.None
 })
-export class LayerSelectorComponent implements OnInit {
-
+export class LayerSelectorComponent implements OnInit, AfterContentInit {
   constructor(private layerService: LayerService) {
+  }
+
+  ngAfterContentInit() {
   }
 
   ngOnInit() {
