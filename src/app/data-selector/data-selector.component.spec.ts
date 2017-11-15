a0a8ec8f3644c3995a9cef3e0a4ab0c358f455c8
Carlos <c.martinez@esciencecenter.nl>
2017-11-15 13:08:53 +0100

    Fix tests breaking

--- src/app/data-selector/data-selector.component.spec.ts
+++ src/app/data-selector/data-selector.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DataSelectorComponent } from './data-selector.component';
@@ -8,7 +9,8 @@ describe('DataSelectorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DataSelectorComponent ]
+      declarations: [ DataSelectorComponent ],
+      schemas:      [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
--- src/app/model-selector/model-selector.component.spec.ts
+++ src/app/model-selector/model-selector.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModelSelectorComponent } from './model-selector.component';
@@ -8,7 +9,8 @@ describe('ModelSelectorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ModelSelectorComponent ]
+      declarations: [ ModelSelectorComponent ],
+      schemas:      [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
